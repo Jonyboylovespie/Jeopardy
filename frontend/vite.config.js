8065a7c3f3b00b5dbc3586c9ccc69941edf192bc
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2026-03-17 21:39:37 -0400

    Add preview configuration to Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // allow any host to connect - use only for local testing
+    allowedHosts: 'all',
+    host: true
+  }
 })
